85f806eec923c9b285cc0345fd75dc2260a1c779
Čeněk Použník <potuznikcenek@gmail.com>
2024-03-06 21:11:18 +0100

    gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '5e72ba27fc6a863eed13c27e67501234ab0be9ff55ac0e34823d966c1ce4896026992f2639857117'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
+app.config['DB_SERVER'] = '127.0.0.1:8963'
+
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 db = SQLAlchemy(app)
