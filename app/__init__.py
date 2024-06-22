c0386272f98ba570bb9afb6fb6e23abeca2957de
Čeněk Použník <potuznikcenek@gmail.com>
2024-06-22 19:10:49 +0200

    added correct master server addr

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '5e72ba27fc6a863eed13c27e67501234ab0be9ff55ac0e34823d966c1ce4896026992f2639857117'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
-app.config['DB_SERVER'] = 'http://127.0.0.1:8963'
+app.config['DB_SERVER'] = 'https://jsnsgekom.cz'
 app.config['CONFIG_FILE_PATH'] = 'app/config.json'
 app.config['CLIENTS_FILE_PATH'] = 'app/clients.json'
 app.config['LOG_ARCHIVE_FOLDER'] = 'app/log_archive/'
