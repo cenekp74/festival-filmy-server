9db23201db9319ff191c7410367a872ddfcafd1b
Čeněk Použník <potuznikcenek@gmail.com>
2024-07-29 18:47:00 +0200

    whoops

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,8 @@ from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5e72ba27fc6a863eed13c27e67501234ab0be9ff55ac0e34823d966c1ce4896026992f2639857117'
+app.secret_key = 'dev'
+app.config.from_pyfile('../instance/config.py')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 app.config['DB_SERVER'] = 'https://jsnsgekom.cz'
